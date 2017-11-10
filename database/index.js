95e334d7d00c1d40560509f39f7b3caf84ab69b9
dwrz <dwrz@dwrz.net>
2017-11-10 21:17:22 +0000

    Basic setup for database.

---
+++ database/index.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const URL = 'mongodb://localhost/log/';
+mongoose.connect(URL);
+
+const entrySchema = mongoose.Schema({
+  timestamp: Date,
+  event: String,
+  description: String
+});
+
+let Entry = mongoose.model('Entry', entrySchema);
+
+function save(entry) {
+  let newEntry = new Entry(entry);
+  newEntry.save(function (error, success) {
+    if (error) {
+      console.error('ERROR: FAILED TO SAVE TO DB');
+      console.error(error);
+    }
+  });
+}
+
+function load(callback) {
+  Entry.find((error, entries) => {
+    if (error) {
+      console.error('ERROR: FAILED TO LOAD FROM DB');
+      console.error(error);
+    } else {
+      callback(entries);
+    }
+  });
+}
+
+exports.save = save;
+exports.load = load;
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const database = require('../database/index.js');
 const express = require('express');
 const app = express();
 const PORT = 3000;
@@ -5,8 +6,20 @@ const PORT = 3000;
 app.get('/', (request, response) => {
   response.send('ROOT');
 });
-app.post('/api/log/entry', (request, response) => {});
-app.get('/api/log/entries/current/day', (request, response) => {});
+
+app.get('/api/log/entries/current/day', (request, response) => {
+  // RETURN JSON -- ARRAY OF OBJECTS
+  console.log('RECEIVED A GET');
+  response.send('RECEIVED YOUR GET');
+});
+
+app.post('/api/log/entry', (request, response) => {
+  console.log('RECEIVED A POST');
+  // STORE ENTRY IN DATABASE
+  // database.save
+  
+  response.send('RECEIVED YOUR POST');
+});
 
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
