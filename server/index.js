e617c3209617f7ff2ee9e7b90e5fdd48fc97f686
dwrz <dwrz@dwrz.net>
2017-11-11 03:20:21 +0000

    Change URLs and ports for production.

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const database = require('../database/index.js');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 
 app.use(express.static(__dirname + '/../client/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
