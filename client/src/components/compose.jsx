de573fc35aa05b4bb913a6aa71fca21799e967aa
dwrz <dwrz@dwrz.net>
2017-11-11 00:39:55 +0000

    Flesh out compose component.

--- client/src/components/compose.jsx
+++ client/src/components/compose.jsx
@@ -6,9 +6,20 @@ class Compose extends React.Component {
   render() {
     return (
       <div>
-        <p>Compose here.</p>
+        <form>
+          <div className="form-group">
+            <label>Event</label>
+            <input id="event" type="text" className="form-control" aria-label="event" aria-describedby="event"/>
+          </div>
+          <div className="form-group">
+            <label>Entry</label>
+            <textarea id="entry" className="form-control" rows="3"></textarea>
+          </div>
+        </form>
+        <div className="text-center">
+          <button id="submit" type="button" className="btn btn-lg btn-success">Submit</button>
+          </div>
       </div>
     );
   }
-  
 }
