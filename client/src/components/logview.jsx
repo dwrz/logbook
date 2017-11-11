965296972e861d00bfb82b27751df6b1358b4a48
dwrz <dwrz@dwrz.net>
2017-11-11 01:15:17 +0000

    Display fetched entries.

--- client/src/components/logview.jsx
+++ client/src/components/logview.jsx
@@ -12,6 +12,13 @@ class LogView extends React.Component {
             <tr className="table-success"><th>Date</th><th>Event</th><th>Description</th></tr>
           </thead>
           <tbody>
+            {this.props.entries.map(entry =>
+              <tr key={entry._id}>
+                <td>{entry.timestamp}</td>
+                <td>{entry.event}</td>
+                <td>{entry.description}</td>
+              </tr>
+            )}
           </tbody>
         </table>
       </div>
