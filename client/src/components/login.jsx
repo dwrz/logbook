3d3712abf211f1c7c2677635f7a0af5a5123fe4d
dwrz <dwrz@dwrz.net>
2017-11-11 23:03:21 +0000

    Add login page and component.

---
+++ client/src/components/login.jsx
@@ -0,0 +1,35 @@
+const serverURL = 'http://127.0.0.1:3000';
+const POST = '/login';
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: ''
+    };
+  }
+
+  onChange(event) {
+    this.setState({
+      username: event.target.value
+    });
+  }
+
+  submit () {
+    let user = {
+      username: this.state.username
+    };
+    $.post(serverURL + POST, user, (response) => {
+      console.log(response);
+    });
+  }
+
+  render() {
+    return (
+      <div className="input-group">
+        <span className="input-group-addon">Username:</span>
+        <input value={this.state.username} onChange={this.onChange.bind(this)} type="text" className="form-control"/><button className="btn btn-success" onClick={this.submit.bind(this)}>Submit </button>
+      </div>
+    );
+  }
+}
